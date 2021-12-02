3d1bee26d880352d141793e4ee0b5d39ecb7fb56
CraigB73 <craigbritton73@gmail.com>
2021-12-02 14:27:59 +0100

    Deleted some unused files and created new component file for robots and cards

---
+++ src/components/SearchBox.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const SearchBox = ({serchfield, searchChange}) => {
+  return (
+    <div className= 'pa2'>
+      <input 
+        className ='pa3 tc ba b--green bg-lightest-blue'
+        type= 'search'
+        placeholder= 'search robots'
+        onChange= {searchChange}
+      />
+    </div>
+    
+  )
+}
+
+export default SearchBox;
---
+++ src/robots.js
@@ -0,0 +1,32 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Hanna',
+    birthdate: 'June 2nd',
+    email: 'hannabritton73@gmai.com'
+  },
+  {
+    id: 2,
+    name: 'Nova',
+    birthdate: 'December 16th',
+    email: 'britton.nova2011@gmail.com'
+  },
+  {
+    id: 3,
+    name: 'Aiden ',
+    birthdate: 'July 28th',
+    email: 'none',
+  },
+  {
+    id: 4,
+    name: 'Candice',
+    birthdate: 'December 16th',
+    email: 'none'
+  },
+  {
+    id: 5,
+    name: 'Samuel',
+    birthdate: 'April 3rd',
+    email: 'samuel.bengtsson@sigmatechnology.se',
+  },
+]
